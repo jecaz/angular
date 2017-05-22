2f703672520ce234b20e9af73e0915cbbe2a8991
Jelena Zobenica <jelena.zobenica1@gmail.com>
2017-05-22 21:36:59 +0200

    Complete comment-list component

--- src/app/component/post-list/post/comment/comment-list/comment-list.component.ts
+++ src/app/component/post-list/post/comment/comment-list/comment-list.component.ts
@@ -11,11 +11,21 @@ import {Post} from '../../../../../../model/post.model';
 export class CommentListComponent implements OnInit {
 
   private post: Post;
+  private id: number
 
   constructor(private postService: PostService, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    const id: number = +this.route.snapshot.params['id'];
-    this.post = this.postService.getPost(id);
+/*    id: number;
+    this.sub = this.route.params.subscribe(params => {
+      this.id = +params['id']; // (+) converts string 'id' to a number
+    });*/
+
+    this.id = +this.route.snapshot.params['id'];
+    this.post = this.postService.getPost(this.id);
   }
+
+ /* ngOnDestroy() {
+    this.sub.unsubscribe();
+  }*/
 }
